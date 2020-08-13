c0369dd4010c341ef1df889fe11e0958dead8b27
Kush pranjale <30398986+kushpranjale@users.noreply.github.com>
2020-08-13 20:24:20 +0530

    kush first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IframeComponent } from './iframe/iframe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IframeComponent
   ],
   imports: [
     BrowserModule,
